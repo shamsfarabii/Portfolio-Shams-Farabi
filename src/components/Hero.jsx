98d7e0018f60a1d6fa4a51e3471512fc2500e8f2
farabi-liberatelabs <farabi@liberate-labs.com>
2023-10-25 13:06:11 +0600

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ export default function Hero() {
                     <img src="https://i.ibb.co/8K0DCVQ/pic.jpg" alt="" className='w-[300px] h-[300px] rounded-full' />
                 </div>
                 <div className='md:w-1/2 flex flex-col items-center justify-center'>
-                    <div className='flex items-center mt-20 md:ml-[100px] animate-fade-left justify-center'>
+                    <div className='flex items-center lg:mt-20 mt-5 md:ml-[100px] animate-fade-left justify-center'>
                         <h1 className='text-3xl mr-[20px] ml-[25px]'>Hi, I am Farabi!</h1>
                         <img className='w-16 mb-5' src="https://media.tenor.com/SNL9_xhZl9oAAAAi/waving-hand-joypixels.gif" alt="" />
                     </div>
-                    <div className='mt-5 ml-10 mb-20 md:mr-0 animate-fade-left text-xl text-center'>
+                    <div className='mt-5 ml-10 mr-10 mb-20 md:mr-0 animate-fade-left text-xl text-center'>
                         <p>
                         I&apos;m a <span className='font-bold text-blue-400'>Software Engineer</span> with a passion for <span className='text-yellow-200'>problem-solving</span> and <span className='text-yellow-200'>innovative thinking</span>. My software engineering exposed me to diverse technical challenges, and I&apos;m committed to continuous learning and knowledge sharing. I thrive on crafting innovative solutions and cultivating a collaborative work atmosphere.
                         </p>
