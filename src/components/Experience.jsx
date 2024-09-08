5b299d044c7c4b7137b4a4649542ea29408bc5dc
shamsfarabii <shams99@arafat.dev>
2024-09-08 21:49:43 +0600

    feat - experience section added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,7 +16,7 @@ export default function Experience() {
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-10 justify-center items-center w-full">
               <div className="flex flex-col justify-center w-2/3 lg:w-1/3 items-center">
                 <img
-                  src="/public/assets/companyLogo/brownbrick.svg"
+                  src="https://previews.dropbox.com/p/thumb/ACaxQMyxEdMAsTCTWWBWhSivw5Ta71RPjT7WG29G_iyCefXH_8LXCRQQQwH9fExH_hjFruYimmAmeikPFI-_kVGh5b5tSPZUCBa-1qA_NQwKEaLoNecwAm4fpXfRXBJ31vPnJvMIrrdpQegMjw_Yss__M7D0U7TnbGG0jD_HOQ1s3LUqI03GB9BTcjzHnVQ5CVyH4nroGi5hXggvvy8dSw3jdBQaUZgUrhMKUIfFjqt06MciQCUurLP53w8ip8d858pWqQBL3CXa6alLKL2tfkC0dlsR-AmBjQSnq08eOC0iRm-W--NPmFOGUBNFze3vAwU/p.png"
                   alt="BrownBrick"
                   width="300"
                   height="300"
@@ -32,7 +32,7 @@ export default function Experience() {
             <div className="flex flex-col lg:flex-row gap-10 lg:gap-10 justify-center items-center w-full">
               <div className="flex flex-col items-center justify-center w-2/3 lg:w-1/3">
                 <img
-                  src="/public/assets/companyLogo/liberate-labs.svg"
+                  src="https://previews.dropbox.com/p/thumb/ACYwf93DSJCCiDFDQil15SPR8RzphFlGztng6Idsq3x9PEaE_A3XuFEFU5vSfggDQbXno7pEB2ktjbdbMZzl28pFxZE2i2f71MTws4YNAXoRzmj-_Q_NdO0aP0qbxI68TnQhm-uAZNxVNg4Umx3BL9ELvQDeMwI5tIouvOPDwJahHHoSZUnNJ5eTNwgHfa4oH6P937EQLJARM3Lm2nV9e1TJS0tUXjAY8C30Hf-wJaKTHtDO6IA6vLZ1cxTuzEvx5iWbeJmcTD61SupsGvcsVM_etkqi4NAmF1Y2Xv09DtJK88tHArPp_4mO22xmuhCmx6s/p.png"
                   alt="Liberate Labs"
                   width="300"
                   height="300"
