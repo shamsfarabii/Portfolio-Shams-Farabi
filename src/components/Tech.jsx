e6332d9fd7630fc1920c687933f82e76f7d8d9fb
shamsfarabii <shams99@arafat.dev>
2024-09-14 00:55:37 +0600

    fix: Changed links

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -23,7 +23,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACYy84J66LjDkWwtWiUsOiQHsCIFIifO-ZATQc908LV1G2iGA_WPwttJEwxEeRHECjLcNMMTV-DsELUiBf1PN8zxC-A0IkJb_LDE0frLdeBJOqTnFvxpFufy96VT35lzGA5lMea74L1NY_lqheXs-A-FZ7P5nAXumRWntHY9ZO-B2mNMx7JCEBjvTW-rms6gahNMmvmVMQREN9XMua7QJ1TXeRAk-lI5IA0T0AntZDLbpUv-Cz34LoCawChLMihZHrdb2ASwrQXpYi5MFpinjk4yK4ENv0xv8iV7XSNc8DBHgjXvFWdtBRfXBKuPBqnn2zk/p.png"
+                src="/public/assets/technologies/next-js.svg"
                 alt="nextJS"
                 width="80"
                 height="80"
@@ -31,7 +31,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACaRqfHGLfx1_Tg7DZxg-IhK9yCjDWT50wmCIlIyiIeYAV7_W_BJWpr-A5wdhzRtWYhJA2ZMGwH2LXljgHzQ6N5fwuOC3H0mhV2Zj5GcNmPWl1-z5IKRPFSbOHJqxFBNWzJK3yp2HA7SG2tUR8GyOrlqCFlSZEPuYDopJpLazHcew2uRWHPxUM_QZnO0i3QedgR3i8OS08EaApTeobkRFGrv34itm1m3iNNndxsZ1cVUYmlzPbwSrc30Ln9TpzjJrqufPUeAmvs00VhnrTKRlfklvaBMUYIlpE_1TaYIbHG1tIb8oV2qkaQ9Ki-7tTfVSqk/p.png"
+                src="/public/assets/technologies/react.svg"
                 alt="react"
                 width="80"
                 height="80"
@@ -39,7 +39,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACYcgikKJGYT3unZcZt3NqpLszNXRh8n429WSJ5LP4Y8N3BGHCVl6Uxbhv3tRusCZQfi0-BGF5O0jmhLz3_7AG--wOZ7XMUqVRucMQzQvd9r9TcoJEEMTEH-HpgxadsQKRCkmeaw2GumhcUWQhIWLyzadViwSC_w18jY7gry11KBXarhSpQ0BKi6azqf7hPJhD3fo77oniPMqZTqwZ9OW9c0iPfOZfCILbp4TqlPLxfBR9U0-ROKgylo26MijYLQwELEaeOEbmgr5fRW3ROzDbGsPTmdfuvPuyGEcHCLSDSifWks1ojXKvBFrWPlMhOIB2M/p.png"
+                src="/public/assets/technologies/expo-1.svg"
                 alt="expo"
                 width="80"
                 height="80"
@@ -47,7 +47,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACbpZfwBD9HenGM7QV2PoGlw00Ujds3zDv2Q4jHiL3jB4m4Cxh-E5XEanUJHFSVhKH_h3VLbIuuPXrJClUS0GJDyv7gdkZ20bgldJfqFOMkUWkGIiS4PDQAwdNya5DSSi8DVxvcgDbpW1bVeRb-39Ak6uTD_bwc0ijZOlloSp1fBmhVI-pm5p9sr5K1cqpJxq-TWfFzIPLBMfrZrY2Vg_h4gkWyV2Po38Vra7elW2GD6qfTJdN2l4w7j8yCP2ruSaM95uE7OL8uzGzTWTop1N1nDFDXeF375WMg7CRDm9RfnZAK6JsCdzhf0qQjO5rWVw48/p.png"
+                src="/public/assets/technologies/fastapi-1.svg"
                 alt="fastAPI"
                 width="80"
                 height="80"
@@ -55,7 +55,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACZQTwslxQvSrClWuc_tQFWkF7dL2SYbopGR_oVFgyXUH2Mk6EvMjf2oTR2VdvKNG8MsyuPsZJCgw11MUYkYyJrVl164ytvCakJOaua91Vb1YeNAVOmmiVC8XWqYixxTeZKIcfkhgLfQqK5yPpJUY4Pn0GdjvmYK56tP6EXIc7qcpU9zceXKNXwPsL7c9p0s7-B5FqsNG2O93drUIYAnC1fDLS8yACe5syWHDFEyvDZ-gnTXVdqqCrW5pMR1tGFPtMEoHxinexesZoXfJEIQ0e_q6wKrKLqXNe0FjIAbRBLlD6SybRuA5vYQtTrkNJVW5KhDjDjsQSG7TpMriorcg9kE/p.png?is_prewarmed=true"
+                src="/public/assets/technologies/postgresql.svg"
                 alt="postgres"
                 width="80"
                 height="80"
@@ -63,7 +63,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACbt41rjtID4eEbLXiTVLI5sRNNVaTeuFWuQIOXTfMu2oe8BZ8GYtUnYCy5qowu0gIjCr1MepCJRIv8yU6Whdfv0wWQorr8QGuJKmiuTTV8CcPGga7-xoPvFwtkfmWnKGOEgLCaxh9fA55IIWJmnjJYXsrvGvyXLMg1ajPwh42z2R4IQHYaHXjIx5pKaF38G7BfWSVLXVTirJdib7h_u-IKotWHy5Td9fJSAp3w1Zo7Vezck43PY2nxsUKwuAwn8cP4MK7hd85aqu-u9-D23x2YTrVrcsUBtQdmGyJjU3Dnox-HAWTuycJK6ikLmchd1A_w/p.png"
+                src="/public/assets/technologies/flutter-logo.svg"
                 alt="flutter"
                 width="60"
                 height="60"
@@ -71,7 +71,7 @@ export default function Tech() {
             </div>
             <div className="flex flex-col items-center gap-2 text-center text-sm">
               <img
-                src="https://previews.dropbox.com/p/thumb/ACaNbqLYJvHrU5TegMcIt6yc-3-UtrLxIdtGXwjJpFG3OS9A_gwH1icjgXz9FOCh3YIRWB7gYZLVvNHUacFxr7O7qLjqsdx1LmIcvKnpkwZfdHpQti7Nq23ZNzjJtfiL7wheWVzepe0xDTmyo7rGTJ6pDmAvxim5rgAmvTtc9BdQCXuFnH3BfGG0xB-mECqJX8ax64xOMqumIdJwn_vhmQpEsWBiLa7eiyy69ZgeHLvNRNVkYFGvfVt2OhpBNhq1cwUQj_KeafLT73FZhd32zrFgqu6WRFUSWg6otO0o-OTrEDuDQusHOOHG726x2eE9b78/p.png"
+                src="/public/assets/technologies/mongo.svg"
                 alt="mongo"
                 width="80"
                 height="80"
